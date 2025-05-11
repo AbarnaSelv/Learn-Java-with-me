4a47d8ddfe52989c207f3214235e331bc0bfe488
Abarna <abarnaselvaganapathy@gmail.com>
2025-05-11 20:48:51 +0530

    Added day 2 practice programs: palindrome check, loops, array max, etc.

---
+++ src/com/abarna/basics/day2/String_PalindromeChecker.java
@@ -0,0 +1,24 @@
+package com.abarna.basics.day2;
+import java.util.Scanner;
+
+public class String_PalindromeChecker {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println(original + " is a palindrome!");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
---
+++ src/com/abarna/basics/day2/UserInputDemo.java
@@ -0,0 +1,19 @@
+package com.abarna.basics.day2;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        
+        System.out.println("Hello " + name + ", you are " + age + " years old.");
+        
+        sc.close();
+    }
+}
